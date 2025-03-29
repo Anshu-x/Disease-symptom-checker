1ffd636dda99bdeaf353065148ea836218be3050
Anshu-x <ansumanreet333@gmail.com>
2025-03-30 03:57:44 +0530

    Set NLTK data directory and download resources to a writable path

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ from flask import Flask, request, jsonify
 import pickle
 import re
 import nltk
+import os
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+
+# Set NLTK data directory to a writable path
+NLTK_DATA_DIR = "/tmp/nltk_data"
+os.makedirs(NLTK_DATA_DIR, exist_ok=True)
+nltk.data.path.append(NLTK_DATA_DIR)
+
+# Download required resources in the specified directory
+nltk.download('punkt', download_dir=NLTK_DATA_DIR)
+nltk.download('stopwords', download_dir=NLTK_DATA_DIR)
 
 # Load trained model and vectorizer
 with open("knn_model.pkl", "rb") as model_file:
@@ -11,10 +21,6 @@ with open("knn_model.pkl", "rb") as model_file:
 
 with open("tfidf_vectorizer.pkl", "rb") as vectorizer_file:
     tfidf_vectorizer = pickle.load(vectorizer_file)
-
-# Download stopwords if not available
-nltk.download('punkt')
-nltk.download('stopwords')
 
 app = Flask(__name__)
 
